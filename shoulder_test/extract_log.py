8753862b8c7d415ea9208b9ebca994de48088edc
verywise0307 <asdfg5346@naver.com>
2025-06-01 17:35:25 +0900

    스테이지별 죽음 추가

--- shoulder_test/extract_log.py
+++ shoulder_test/extract_log.py
@@ -10,16 +10,40 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 log_path = os.path.join(script_dir, 'Saved', 'Logs', 'shoulder_test.log')
 
 # 결과 저장 리스트
+log_start = []
 death_counts = []
+stage1death_counts = []
+stage2death_counts = []
+stage3death_counts = []
 
 # 로그 파일 열기
 with open(log_path, 'r', encoding='utf-8', errors='ignore') as file:
     for line in file:
-        # deathcount : 숫자 형태를 찾는 정규표현식
+
+        match = re.search(r'logstart\s*[:：]?\s*(\d{4}-\d{2}-\d{2}-\d{2}:\d{2}:\d{2})', line)
+        if match:
+            time_string = match.group(1)
+            log_start.append(time_string)
+
         match = re.search(r'deathcount\s*:\s*(\d+)', line)
         if match:
             count = int(match.group(1))
             death_counts.append(count)
+        
+        match = re.search(r'stage1deathcount\s*:\s*(\d+)', line)
+        if match:
+            count = int(match.group(1))
+            stage1death_counts.append(count)
+        
+        match = re.search(r'stage2deathcount\s*:\s*(\d+)', line)
+        if match:
+            count = int(match.group(1))
+            stage2death_counts.append(count)
+        
+        match = re.search(r'stage3deathcount\s*:\s*(\d+)', line)
+        if match:
+            count = int(match.group(1))
+            stage3death_counts.append(count)
 
 # 폴더 경로 생성
 output_dir = os.path.join(script_dir, 'exeloutput')
@@ -27,8 +51,21 @@ output_dir = os.path.join(script_dir, 'exeloutput')
 # 엑셀 저장 경로 지정
 excel_path = os.path.join(output_dir, 'DeathCounts.xlsx')
 
+print("리스트 길이 확인")
+print("log_start 길이:", len(log_start))
+print("death_counts 길이:", len(death_counts))
+print("stage1death_counts 길이:", len(stage1death_counts))
+print("stage2death_counts 길이:", len(stage2death_counts))
+print("stage3death_counts 길이:", len(stage3death_counts))
+
 # pandas DataFrame으로 정리
-df = pd.DataFrame({'Death Count': death_counts})
+df = pd.DataFrame({
+    'Log Start': log_start,
+    'Total Death Count': death_counts,
+    'Stage1 Death Count': stage1death_counts,
+    'Stage2 Death Count': stage2death_counts,
+    'Stage3 Death Count': stage3death_counts
+})
 
 # Excel로 저장
 df.to_excel(excel_path, index=False)
